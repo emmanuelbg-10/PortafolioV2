71d3e633e95d52cf178d54c0bec43d1f0ec880b5
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-03-06 05:15:58 +0000

    Actualizar estilo de presentación: cambiar el peso de la fuente del saludo a negrita

--- src/app/Presentation.tsx
+++ src/app/Presentation.tsx
@@ -1,7 +1,7 @@
 export default function Presentation() {
   return (
     <section className="presentation" id="conocimientos">
-      <h1 className="text-3xl font-semibold">Hola</h1>
+      <h1 className="text-3xl font-bold">Hola</h1>
       <p className="text-lg">
         Soy Emmanuel, un desarrollador web
         <br /> que le encanta aprender cosas nuevas.
