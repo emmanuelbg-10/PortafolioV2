e0a9c4a19ff0fbc0a3e4c978851644fe9d819d5b
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-03-07 04:46:36 +0000

    Actualizar descripciones de proyectos destacados y reemplazar imágenes

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -2,7 +2,7 @@ const featuredProjects = [
   {
     id: 1,
     title: "Meteorología",
-    description: "Descripción breve del Proyecto Uno.",
+    description: "Aplicación web de meteorología que consulta la API de El Tiempo para mostrar previsiones actuales y futuras, permitiendo seleccionar provincia y municipio. Interfaz limpia, intuitiva y responsive para una experiencia óptima en cualquier dispositivo.",
     repoUrl: "https://github.com/emmanuelbg-10/Meteorologia",
     liveUrl: "https://meteorologiabg10.vercel.app",
     imageUrl: `/img/Meteorologia.png`,
@@ -10,7 +10,7 @@ const featuredProjects = [
   {
     id: 2,
     title: "Destinos Turísticos",
-    description: "Descripción breve del Proyecto Dos.",
+    description: "Aplicación de planificación de viajes en Vue 3 que permite estimar presupuestos personalizados y visualizar destinos en mapas interactivos, con una interfaz intuitiva y responsive para cualquier dispositivo.",
     repoUrl: "https://github.com/emmanuelbg-10/clienteDestinos",
     liveUrl: "https://destinosbg10.vercel.app/",
     imageUrl: `/img/DestinosTuristicos.png`,
