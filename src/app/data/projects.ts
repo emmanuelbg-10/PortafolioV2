e244d5e8f9634f999299037c7f05fa05a12a553c
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-03-07 04:16:09 +0000

    Actualizar títulos y enlaces de proyectos destacados en la sección de proyectos

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,7 +1,7 @@
 const featuredProjects = [
   {
     id: 1,
-    title: "Proyecto Uno",
+    title: "Meteorología",
     description: "Descripción breve del Proyecto Uno.",
     repoUrl: "https://github.com/emmanuelbg-10/Meteorologia",
     liveUrl: "https://meteorologiabg10.vercel.app",
@@ -9,28 +9,28 @@ const featuredProjects = [
   },
   {
     id: 2,
-    title: "Proyecto Dos",
+    title: "Destinos Turísticos",
     description: "Descripción breve del Proyecto Dos.",
-    repoUrl: "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
-    liveUrl: "https://emmanuelbg10.vercel.app",
-    imageUrl: `/img/2.png`,
+    repoUrl: "https://github.com/emmanuelbg-10/clienteDestinos",
+    liveUrl: "https://destinosbg10.vercel.app/",
+    imageUrl: `/img/DestinosTuristicos.png`,
   },
-  {
-    id: 3,
-    title: "Proyecto Tres",
-    description: "Descripción breve del Proyecto Tres.",
-    repoUrl: "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
-    liveUrl: "https://emmanuelbg10.vercel.app",
-    imageUrl: `/img/2.png`,
-  },
-  {
-    id: 4,
-    title: "Proyecto Cuatro",
-    description: "Descripción breve del Proyecto Cuatro.",
-    repoUrl: "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
-    liveUrl: "https://emmanuelbg10.vercel.app",
-    imageUrl: `/img/2.png`,
-  },
+  // {
+  //   id: 3,
+  //   title: "Proyecto Tres",
+  //   description: "Descripción breve del Proyecto Tres.",
+  //   repoUrl: "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
+  //   liveUrl: "https://emmanuelbg10.vercel.app",
+  //   imageUrl: `/img/2.png`,
+  // },
+  // {
+  //   id: 4,
+  //   title: "Proyecto Cuatro",
+  //   description: "Descripción breve del Proyecto Cuatro.",
+  //   repoUrl: "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
+  //   liveUrl: "https://emmanuelbg10.vercel.app",
+  //   imageUrl: `/img/2.png`,
+  // },
   // Agrega los demás proyectos aquí
 ];
 
